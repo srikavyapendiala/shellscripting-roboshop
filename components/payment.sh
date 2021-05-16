37f4fb44a7f133e5218e0ee7e2613e4b8118c929
srikavyapendiala <srikavya@zelarsoft.com>
2021-05-16 16:50:34 +0530

    #1 Update

--- components/payment.sh
+++ components/payment.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
-
-Head "Create a user for running the application"
-useradd -m -s /bin/bash roboshop
-Stat $?
-
-Head "Switch to Directory"
-$ cd /home/roboshop
-Stat $?
-
-DOWNLOAD_COMPONENT
-
-Head "Unzip and move"
-$ unzip /tmp/payment.zip &>>$LOG
-$ mv payment-main payment
-
 source components/common.sh
 OS_PREREQ
+
+Head "Adding robhoshop user"
+if [$? -ne 0]; then
+   useradd -m -s /bin/bash roboshop
+   Stat $?
+fi
+
+DOWNLOAD_COMPONENT
 
 Head "Install Python Dependency Installer"
 apt install python3-pip -y &>>$LOG
 Stat $?
 
-Head "Install the dependencies"
-cd /home/roboshop/payment
-pip3 install -r requirements.txt &>>$LOG
+Head "Extract Downloaded archieve"
+cd /home/roboshop && unzip /tmp/payment.zip &>>$LOG && mv payment-main payment &&cd /home/roboshop/payment
+&& pip3 install -r requirements.txt &>>$LOG && chown roboshop:roboshop /home/roboshop -R
+Stat $?
 
 Head "Change the Ip address In service files"
 mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service
