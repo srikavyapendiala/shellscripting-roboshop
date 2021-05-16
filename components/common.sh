f2d4b36b112f0e916eb0f0d431b65d0a624fbbed
srikavyapendiala <srikavya@zelarsoft.com>
2021-05-16 19:04:01 +0530

    #1 Update

--- components/common.sh
+++ components/common.sh
@@ -2,7 +2,7 @@
 
 Heading() {
   echo -e "\e[1;36m =================$1===============\e[0m"
-  echo -e "\e[1;36m =================$1===============\e[0m"
+  echo -e "\e[1;36m =================$1===============\e[0m" &>>$LOG
 }
 
 Stat() {
@@ -10,18 +10,21 @@ Stat() {
     echo -e "\e[1;32m" SUCCESS\e[0m"
   else
     echo -e "\e[1;31m" FAILURE\e[0m"
-    echo -e "\n\e[1;33m" we can refer log file log file path = ${LOG}\e[0m"
+    echo -e "\n\e[1;33m" we can refer log file log file path, log file path = ${LOG}\e[0m"
   fi
+
 OS_PREREQ() {
   set-hostname ${component}
   Head "Updating APT repos"
   apt update &>>$LOG
 }
+
 ERROR() {
   echo -e "\e[1;31m$1\e[0m"
 }
+
 DOWNLOAD_COMPONENT() {
   Head "Downloading ${COMPONENT} component"
-  curl -s -L -o /tmp/${COMPONENT}.zip/ https://github.com/srikavyapendiala/shellscripting-roboshop/${COMPONENT}
-  /archieve/main.zip
+  curl -s -L -o /tmp/${COMPONENT}.zip/ "https://github.com/srikavyapendiala/shellscripting-roboshop/${COMPONENT}
+  /archieve/main.zip"
 }
