cbce64437673fb20fe0252bfac77da8a18646812
srikavyapendiala <srikavya@zelarsoft.com>
2021-05-16 16:30:05 +0530

    #1 Update

--- components/rabbitmq.sh
+++ components/rabbitmq.sh
@@ -3,15 +3,29 @@ source components/common.sh
 OS_PREREQ
 
 Head "Install RabbtiMQ"
-curl -fsSL https://github.com/rabbitmq/signing-keys/releases/download/2.0/rabbitmq-release-signing-key.asc | apt-key add -
-apt install apt-transport-https -y &>>$LOG && echo -e "deb https://dl.bintray.com/rabbitmq-erlang/debian focal erlang\ndeb https://dl.bintray.com/rabbitmq/debian bionic main" > /etc/apt/sources.list.d/bintray.rabbitmq.list
+curl -fsSL https://github.com/rabbitmq/signing-keys/releases/download/2.0/rabbitmq-release-signing-key.asc | apt-key
+add -
+Stat $?
+
+Head "Setup Rabbitmq Resources"
+apt install apt-transport-https -y &>>$LOG && echo -e "deb https://dl.bintray.com/rabbitmq-erlang/debian
+focal erlang\ndeb https://dl.bintray.com/rabbitmq/debian bionic main" > /etc/apt/sources.list.d/bintray.rabbitmq.list
 Stat $?
 
 Head "Update all Dependencies"
-apt update -y
-apt install rabbitmq-server -y --fix-missing
+apt update -y &>>$LOG
+Stat $?
+
+Head "Install Rabbiymq"
+apt install rabbitmq-server -y --fix-missing &>>$LOG
+Stat $?
 
 Head "create one user for the application"
-rabbitmqctl add_user roboshop roboshop123 && rabbitmqctl set_user_tags roboshop administrator
-&& rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl list_users | grep roboshop &>>$LOG
+if [ $? -ne 0]; then
+  rabbitmqctl add_user roboshop roboshop123 && rabbitmqctl set_user_tags roboshop administrator
+  && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG
+else
+  rabbitmqctl set_user_tags roboshop administrator &>>$LOG && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+  &>>$LOG
 
--- components/shipping.sh
+++ components/shipping.sh
@@ -7,21 +7,17 @@ Head "Install maven"
 apt install maven -y &>>$LOG
 
 
-Head "Create a user"
-useradd -m -s /bin/bash roboshop
-Stat $?
-
-Head "switch to directory"
-$ cd /home/roboshop
+Head "Adding a user"
+if [$? -ne 0]; then
+   useradd -m -s /bin/bash roboshop
+   Stat $?
+fi
 
 DOWNLOAD_COMPONENT
 
-Head "Unzip all files"
-$ unzip /tmp/shipping.zip
-$ mv shipping-main shipping
-$ cd shipping
-$ mvn clean package
-$ mv target/shipping-1.0.jar shipping.jar
+Head "Extract Downloaded Archieve"
+cd /home/roboshop && unzip /tmp/shipping.zip &>>$LOG  && mv shipping-main shipping && cd /home/roboshop/shipping
+&& mvn clean package &>>$LOG && chown roboshop:roboshop /home/roboshop -R && mv target/shipping-1.0.jar shipping.jar &>>$LOG
 Stat $?
 
 Head "Copy the service file"
