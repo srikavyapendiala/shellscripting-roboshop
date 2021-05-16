abe66b4b340e21d6e4035a78e8b1316222a69e78
srikavyapendiala <srikavya@zelarsoft.com>
2021-05-16 17:09:51 +0530

    #1 Update

--- components/payment.sh
+++ components/payment.sh
@@ -23,7 +23,18 @@ Head "Change the Ip address In service files"
 mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service
 Stat $?
 
+USER_ID=$(id -u roboshop)
+GROUP_ID=$(id -g roboshop)
+
+Head"Update payment Config"
+sed -i -e "/^uid/ c uid = ${USER_ID}" -e "/^gid/ c uid = ${GROUP_ID}" /home/roboshop/payment/payment.ini
+Stat $?
+
+Head"Update Endpoints In Service file"
+sed -i -e "s/CARTHOST/cart.kavya.website/" -e "s/USERHOST/user.kavya.website/" -e "s/AMPQOST/rabbitmq.kavya.website/"
+Stat $?
+
 Head "Setup the service"
-systemctl daemon-reload
+mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.ssservice && systemctl daemon-reload
 && systemctl enable payment && systemctl start payment
 Stat $?
