c17c248bf5c87463f21f035c137a493fdfba04b5
srikavyapendiala <srikavya@zelarsoft.com>
2021-05-16 16:12:51 +0530

    #1 Update

--- components/mysql.sh
+++ components/mysql.sh
@@ -10,12 +10,25 @@ Head"Enable and start Docker"
 systemctl enable docker && systemctl start docker
 Stat $?
 
-Head "Start MySQL Container and also should be accessible over network"
-mkdir /data
-docker run -v /data:/var/lib/mysql -p 3306:3306 -d -e MYSQL_ROOT_PASSWORD=password mysql:5.7.33
+Head "Start MySQL with docker"
+mkdir -p /data
+docker ps | grep mysql -i &>>$LOG
+if [ $? -ne 0]; then
+  docker run -v /data:/var/lib/mysql -p 3306:3306 -d -e MYSQL_ROOT_PASSWORD=password mysql:5.7.33 &>>$LOG
+if
 Stat $?
 
 DOWNLOAD_COMPONENT
 
-Head " switch to directory and unzip"
-cd /tmp && unzip /tmp/mysql.zip && cd mysql-main && mysql -h <IP-ADDRESS> -uroot -ppassword <shipping.sql
+Head "Extract Downloaded Schema"
+cd /tmp && unzip /tmp/mysql.zip &>>$LOG
+Stat $?
+
+Head"Install MYSQL Client"
+apt install maradb-client-core -y &>>$LOG
+
+Head "Load Schema "
+cd mysql-main
+IPADDR=$(hostname -i)
+mysql -h <IP-ADDRESS> -uroot -ppassword <shipping.sql &>>$LOG
+Stat $?
